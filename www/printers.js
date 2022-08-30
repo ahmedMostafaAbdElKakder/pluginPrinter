de7af5dd059ae3755f7d913f1f1f333eaeb4e5ff
ahmed mostafa <ahmed.mostafa@filnady.app>
2022-08-30 15:18:47 +0200

    edit

--- www/printers.js
+++ www/printers.js
@@ -1,5 +1,18 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
+function Inject(){
+}
+Inject.prototype.coolMethod = function (arg0, success, error) {
     exec(success, error, 'printers', 'coolMethod', [arg0]);
 };
+
+Inject.install = function () {
+  if (!window.plugins) {
+    window.plugins = {};
+  }
+
+  window.plugins.Inject = new Inject();
+  return window.plugins.Inject;
+};
+
+cordova.addConstructor(Inject.install);
